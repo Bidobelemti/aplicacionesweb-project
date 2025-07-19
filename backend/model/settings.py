b2ea7981c745281ad7a1eb867d4f0dd1a5923b0c
Bidobelemti <mauriceuve3221@gmail.com>
2025-07-19 09:40:39 -0500

    Corrección de paths para inicialización de django

--- backend/model/settings.py
+++ backend/model/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'model.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'model.wsgi.application'
 
 
 # Database
