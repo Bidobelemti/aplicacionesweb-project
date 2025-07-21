e2890d35560a2833650d682359f29910357cee3c
Bidobelemti <mauriceuve3221@gmail.com>
2025-07-20 19:44:42 -0500

    Configuración sobre serializador, urls, models y views para corrección de post

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -1,7 +1,23 @@
 from django.urls import path
-from .views import OrderListView, api_info
+from . import views
 urlpatterns = [
-    path('', api_info, name='api_info'),  # GET /api/users/
-    path('orders/', OrderListView.as_view(), name='order-list'),
+    path('', views.api_info, name='api_info'),  # GET /api/users/
+    path('orders/', views.OrderListView.as_view(), name='order-list'),
+    path('create/', views.create_order_with_items, name='create-order-with-items'),
+    path('orders/<str:order_type>/<int:order_id>/status/', views.update_order_status, name='update-order-status'),
+    path('menu/', views.MenuItemListCreateView.as_view(), name='menu-item-list-create'), # GET/POST /api/orders/menu/
 
+    
+    # URLs para pedidos en mesa
+    path('eatin/', views.EatInOrderListCreateView.as_view(), name='eatin-order-list'),
+    path('eatin/<int:pk>/', views.EatInOrderDetailView.as_view(), name='eatin-order-detail'),
+    
+    # URLs para pedidos para llevar
+    path('takeaway/', views.TakeAwayOrderListCreateView.as_view(), name='takeaway-order-list'),
+    path('takeaway/<int:pk>/', views.TakeAwayOrderDetailView.as_view(), name='takeaway-order-detail'),
+    
+    # URLs para pedidos con envío
+    path('shipping/', views.ShippingOrderListCreateView.as_view(), name='shipping-order-list'),
+    path('shipping/<int:pk>/', views.ShippingOrderDetailView.as_view(), name='shipping-order-detail'),
+    
 ]
